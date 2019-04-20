ee8918e4712563f299487857025bbfb35615e2c9
Dopesky <oboke69@gmail.com>
2019-04-20 13:46:56 +0300

    Made changes to develop from master

--- index.php
+++ index.php
@@ -313,5 +313,4 @@ switch (ENVIRONMENT)
  *
  * And away we go...
  */
-exec("ln -s ".FCPATH.'vendor/owasp/csrf-protector-php/js/csrfprotector.js'." ".FCPATH."assets/js/csrfprotector/csrfprotector.js");
 require_once BASEPATH.'core/CodeIgniter.php';
