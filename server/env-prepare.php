db003eb5518077ec0278c3ca56b77e75a5da50d0
Dopesky <oboke69@gmail.com>
2019-05-05 05:12:26 +0300

    Solved Deploy Error for not composer installing

--- server/env-prepare.php
+++ server/env-prepare.php
@@ -4,7 +4,7 @@
 * not exist it assumes the environment variables in the .env_example are saved on the server environment. Ensure this is so 
 * since some core functionalities like email delivery depend on this setting. 
 */
-require_once "vendor/autoload.php";
+require_once "../vendor/autoload.php";
 if(file_exists('.env')){
 	$dotenv = Dotenv\Dotenv::create(__DIR__);
 	$dotenv->load();
